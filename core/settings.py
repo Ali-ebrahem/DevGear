fea0dc886b48f6ebd47690a851b9736d6aa38fed
ali ebrahem <alielba09@gmail.com>
2026-04-14 22:32:07 +0200

    fix media images

--- core/settings.py
+++ core/settings.py
@@ -129,6 +129,8 @@ ALLOWED_HOSTS = [
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
+MEDIA_URL = '/media/'
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
